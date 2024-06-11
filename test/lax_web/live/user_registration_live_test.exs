481a5a00502e988e9959415db7324783ca0a7356
Graham Preston <graham.preston@gmail.com>
2024-06-10 21:02:25 -0400

    Fix issue with setting hidden input
    This broke after updating the user fixture due to https://github.com/phoenixframework/phoenix_live_view/issues/988

--- test/lax_web/live/user_registration_live_test.exs
+++ test/lax_web/live/user_registration_live_test.exs
@@ -41,8 +41,8 @@ defmodule LaxWeb.UserRegistrationLiveTest do
       {:ok, lv, _html} = live(conn, ~p"/users/register")
 
       email = unique_user_email()
-      form = form(lv, "#registration_form", user: valid_user_attributes(email: email))
-      render_submit(form)
+      form = form(lv, "#registration_form", user: valid_user_attributes(email: email, time_zone: ""))
+      render_submit(form, %{"user" => %{"time_zone" => "Europe/Paris"}})
       conn = follow_trigger_action(form, conn)
 
       assert redirected_to(conn) == ~p"/"
