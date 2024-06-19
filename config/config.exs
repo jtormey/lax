4ff71ec1f28aa2b7681200ea0233ffc27e41f948
Brian Cardarella <bcardarella@gmail.com>
2024-06-19 14:48:44 -0400

    LVN setup

--- config/config.exs
+++ config/config.exs
@@ -63,6 +63,31 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+config :live_view_native, plugins: [
+  LiveViewNative.SwiftUI
+]
+
+config :mime, :types, %{
+  "text/swiftui" => ["swiftui"],
+  "text/styles" => ["styles"]
+}
+
+config :phoenix_template, :format_encoders, [
+  swiftui: Phoenix.HTML.Engine
+]
+
+config :phoenix, :template_engines, [
+  neex: LiveViewNative.Engine
+]
+
+config :live_view_native_stylesheet,
+  content: [
+    swiftui: [
+      "lib/**/*swiftui*"
+    ]
+  ],
+  output: "priv/static/assets"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
---
+++ native/swiftui/Lax/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Lax
+//
+
+import SwiftUI
+import LiveViewNative
+import LiveViewNativeLiveForm
+
+struct ContentView: View {
+    var body: some View {
+        #LiveView(
+            .automatic(
+                development: .localhost(path: "/"),
+                production: URL(string: "https://example.com")!
+            ),
+            addons: [
+                LiveFormRegistry<_>.self
+            ]
+        ) {
+            ConnectingView()
+        } disconnected: {
+            DisconnectedView()
+        } reconnecting: { content, isReconnecting in
+            ReconnectingView(isReconnecting: isReconnecting) {
+                content
+            }
+        } error: { error in
+            ErrorView(error: error)
+        }
+    }
+}
