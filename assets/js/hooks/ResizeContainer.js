c6e8fed498cdaa39a67c71c0043927f19bd366d9
Justin Tormey <jrtormey@gmail.com>
2024-07-24 17:39:06 -0500

    Support resizing container from left side

--- assets/js/hooks/ResizeContainer.js
+++ assets/js/hooks/ResizeContainer.js
@@ -4,27 +4,27 @@ const BORDER_SIZE = 4;
 const ResizeContainer = {
   mounted() {
     this.pushResize = debounce(this.pushResize.bind(this), 100);
-    this.onMousemove = this.onMousemove.bind(this);
+    this.onDocumentMousemove = this.onDocumentMousemove.bind(this);
     this.onMouseup = this.onMouseup.bind(this);
 
-    this.el.addEventListener("mouseover", this.onMouseover.bind(this));
+    this.el.addEventListener("mousemove", this.onMousemove.bind(this));
     this.el.addEventListener("mouseleave", this.onMouseleave.bind(this));
     this.el.addEventListener("mousedown", this.onMousedown.bind(this));
   },
 
   destroyed() {
     // clean up global event listeners
-    document.removeEventListener("mousemove", this.onMousemove, false);
-    document.removeEventListener("mousemove", this.onMousemove, false);
+    document.removeEventListener("mousemove", this.onDocumentMousemove, false);
+    document.removeEventListener("mousemove", this.onDocumentMousemove, false);
   },
 
   pushResize(width) {
     this.pushEvent(this.el.getAttribute("phx-resize") || "resize", { width });
   },
 
-  onMouseover(event) {
+  onMousemove(event) {
     // hover isn't supported for pseudo elements, so manually change opacity on mouseover
-    if (this.el.getBoundingClientRect().width - event.offsetX < BORDER_SIZE) {
+    if (this.detectInDragArea(event)) {
       this.el.style.setProperty('--resize-indicator-opacity', 1);
     } else {
       this.el.style.setProperty('--resize-indicator-opacity', 0);
@@ -36,18 +36,20 @@ const ResizeContainer = {
   },
 
   onMousedown(event) {
-    if (this.el.getBoundingClientRect().width - event.offsetX < BORDER_SIZE) {
+    if (this.detectInDragArea(event)) {
       this.mouseX = event.x;
 
       document.body.classList.add("select-none");
 
-      document.addEventListener("mousemove", this.onMousemove, false);
+      document.addEventListener("mousemove", this.onDocumentMousemove, false);
       document.addEventListener("mouseup", this.onMouseup, false);
     }
   },
 
-  onMousemove(event) {
-    const dx = this.mouseX - event.x;
+  onDocumentMousemove(event) {
+    const isLeft = this.el.classList.contains("resize-container-left");
+    const dx = isLeft ? event.x - this.mouseX : this.mouseX - event.x;
+
     this.mouseX = event.x;
 
     let width = this.el.getBoundingClientRect().width - dx;
@@ -64,8 +66,20 @@ const ResizeContainer = {
     document.body.classList.remove("select-none");
 
     // clean up global event listeners
-    document.removeEventListener("mousemove", this.onMousemove, false);
-    document.removeEventListener("mouseup", this.onMousemove, false);
+    document.removeEventListener("mousemove", this.onDocumentMousemove, false);
+    document.removeEventListener("mouseup", this.onDocumentMousemove, false);
+  },
+
+  detectInDragArea(event) {
+    if (this.el.classList.contains("resize-container-left")) {
+      return event.offsetX < BORDER_SIZE;
+    }
+
+    if (this.el.classList.contains("resize-container-right")) {
+      return this.el.getBoundingClientRect().width - event.offsetX < BORDER_SIZE;
+    }
+
+    throw new Error("Missing container class");
   }
 }
 
